8b42c21f475fad37d45e89f024b9c03430224dc6
ColinMcLaughlin <109684991+ColinMcLaughlin@users.noreply.github.com>
2025-10-31 21:35:21 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hi Cait</h1>
     <p>I hope you have a good time in Nueva YOOOOOOL.</p>
-    <img src="https://images.pexels.com/photos/1955134/pexels-photo-1955134.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Bad Bunny" style="max-width: 500px; border-radius: 8px;">
+    <img src="https://images.pexels.com/photos/3945683/pexels-photo-3945683.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Bad Bunny" style="max-width: 500px; border-radius: 8px;">
   </div>
 `
