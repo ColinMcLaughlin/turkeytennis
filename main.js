b315f1eafbf21dd2ff5fad61762a981ba8561779
ColinMcLaughlin <109684991+ColinMcLaughlin@users.noreply.github.com>
2025-10-31 21:55:36 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hi Cait</h1>
-    <p>I hope you have a good time in Nueva YOOOOOOL.</p>
+    <h1>Tennis</h1>
+    <p>Turkey Tennis Invitational.</p>
     <img src="https://images.pexels.com/photos/3945683/pexels-photo-3945683.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Bad Bunny" style="max-width: 500px; border-radius: 8px;">
   </div>
 `
