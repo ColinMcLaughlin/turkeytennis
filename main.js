9fccdfc8d11457f793760701b79699016017dc96
ColinMcLaughlin <p.mclaughlin.colin@gmail.com>
2025-11-01 22:37:57 -0400

    remove image

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Tennis</h1>
     <p>Turkey Tennis Invitational.</p>
-    <img src="https://images.pexels.com/photos/3945683/pexels-photo-3945683.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Bad Bunny" style="max-width: 500px; border-radius: 8px;">
   </div>
 `
